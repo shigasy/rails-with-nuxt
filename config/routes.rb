e00d619c9067514085c755d77414c80a9398c6ea
MyPoZi <kxx555u.u@gmail.com>
2019-11-30 01:48:54 +0900

    add helloAPI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
+  namespace :api do
+    namespace :v1 do
+      resources :hello, only:[:index]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
